cc077bcba2aa6d960e0191c248af07851a906f68
TueDej <soweeeso@gmail.com>
2026-06-29 01:06:44 +0330

    feat: update install.sh with MY_USER_ID

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 set -e
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <your-varden-bot-token> [gf-user-id]"
+    echo "Usage: $0 <your-varden-bot-token> [gf-user-id] [my-user-id]"
     exit 1
 fi
 
 TOKEN=$1
 GF_ID=${2:-190637471}
+MY_ID=${3:-2059317327}
 
 sudo apt update && sudo apt install -y python3 python3-pip python3-venv
 
@@ -25,6 +26,7 @@ WorkingDirectory=$(pwd)
 ExecStart=$(pwd)/venv/bin/python3 bot.py
 Environment=TELEGRAM_BOT_TOKEN=$TOKEN
 Environment=GF_USER_ID=$GF_ID
+Environment=MY_USER_ID=$MY_ID
 Restart=always
 
 [Install]
